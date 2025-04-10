801cbda938ad322fbee3cfb042411047ca170817
Elisha Kiley <bockey1999@hotmail.com>
2025-04-10 17:34:24 +0100

    Test 3 - TS Errors

--- src/components/ReviewComponent.vue
+++ src/components/ReviewComponent.vue
@@ -3,22 +3,52 @@
     <h2>{{ t('review.title') }}</h2>
     <textarea v-model="reviewText" :placeholder="t('review.placeholder')"></textarea>
     <button @click="submitReview">{{ t('review.submit') }}</button>
+    <div>
+      <p>{{ someVariable }}</p> 
+      <p>{{ implicitlyTypedVariable }}</p> 
+    </div>
+ 
+    <div>
+      <button @click="createClassInstance">Create Class Instance</button>
+    </div>
   </div>
 </template>
 
 import { ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 <script lang="ts">
+  
+let someVariable: any = "This is an any type variable"; 
 
-const { t } = useI18n();
-const reviewText = ref('');
 
-function submitReview() {
+let implicitlyTypedVariable = 42;  
+
+function submitReview() {   
   if (reviewText.value.trim()) {
     alert(`Review submitted: ${reviewText.value}`);
     reviewText.value = '';
   }
 }
+
+let unusedVariable = "This is an unused variable"; 
+
+
+class ExampleClass {
+  public property: string;  
+  
+  constructor() {
+ 
+  }
+}
+
+
+function createClassInstance() {
+  const example = new ExampleClass(); 
+}
+
+
+const { t } = useI18n();
+const reviewText = ref<string>(''); 
 </script>
 
 <style scoped>
